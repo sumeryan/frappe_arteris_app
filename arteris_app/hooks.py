fad65ea240145f9c148713f0fb01f948a5e11972
igor-sumeryan <igor@sumeryan.com>
2025-04-24 21:39:01 +0000

    Primeiro commit

--- arteris_app/hooks.py
+++ arteris_app/hooks.py
@@ -11,15 +11,15 @@ app_license = "mit"
 # required_apps = []
 
 # Each item in the list will be shown as an app in the apps page
-# add_to_apps_screen = [
-# 	{
-# 		"name": "arteris_app",
-# 		"logo": "/assets/arteris_app/logo.png",
-# 		"title": "Arteris",
-# 		"route": "/arteris_app",
-# 		"has_permission": "arteris_app.api.permission.has_app_permission"
-# 	}
-# ]
+add_to_apps_screen = [
+	{
+		"name": "arteris_app",
+		"logo": "/assets/arteris_app/logo.png",
+		"title": "Arteris",
+		"route": "/arteris_app",
+		# "has_permission": "arteris_app.api.permission.has_app_permission"
+	}
+]
 
 # Includes in <head>
 # ------------------
