2af62b997bb352b6c3b523a9b6b165e17ab0ce3b
igor-sumeryan <igor@sumeryan.com>
2025-04-27 20:45:14 +0000

    Ajustes

---
+++ arteris_app/arteris/doctype/contract_measurement_work_role/contract_measurement_work_role.py
@@ -0,0 +1,28 @@
+# Copyright (c) 2025, Renoir and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+# class ContractMeasurementWorkRole(Document):
+# 	pass
+
+class ContractMeasurementWorkRole(Document):
+    def validate(self):
+        # Verificar se temos os campos necessários preenchidos
+        if self.item and self.funcao:
+            # Buscar o valor de funcao na tabela filha Contract Item Work Role
+            # filtrando pelo item e funcao do documento atual
+            funcoes_item = frappe.get_all(
+                "Contract Item Work Role",  # Nome do DocType da tabela filha
+                filters={
+                    "parent": self.item,       # Item selecionado no documento atual
+                    "funcao": self.funcao      # Função selecionada no documento atual
+                },
+                fields=["name"]
+            )
+            
+            # Se encontrou algum resultado, atribui ao campo funcaoitem
+            if funcoes_item and len(funcoes_item) > 0:
+                self.funcaoitem = funcoes_item[0].name
