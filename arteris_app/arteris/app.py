2f9b2740328b741a2f41e01214f6f500978460de
igor-sumeryan <igor@sumeryan.com>
2025-05-22 12:05:32 +0000

    ...

--- arteris_app/arteris/app.py
+++ arteris_app/arteris/app.py
@@ -1,8 +1,8 @@
-# import frappe
+import frappe
 
-# @frappe.whitelist(allow_guest=True)
-# def get_ping():
-#     return "pong"
+@frappe.whitelist(allow_guest=True)
+def get_ping():
+    return "pong"
 
 
 # @frappe.whitelist(allow_guest=True)
