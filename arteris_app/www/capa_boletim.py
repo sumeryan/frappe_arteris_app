a34881b9107b5dfce57381e9eebe9707a34166b3
igor-sumeryan <igor@sumeryan.com>
2025-06-24 18:05:21 +0000

    get capa data from frappe

--- arteris_app/www/capa_boletim.py
+++ arteris_app/www/capa_boletim.py
@@ -1,5 +1,6 @@
 import frappe
 from frappe import _
+import json
 
 def get_context(context):
     context.no_cache = 1
@@ -7,7 +8,18 @@ def get_context(context):
     doc_id = frappe.form_dict.get('id')
     if doc_id:
         try:
-            context.doc = frappe.get_doc('Contract Measurement', doc_id)
+            doc = frappe.get_doc('Contract Measurement', doc_id)
+            context.contract_measurement_json = frappe.as_json(doc.as_dict())
+
+            contract = frappe.get_doc('Contract', doc.contrato)
+            context.contract_json = frappe.as_json(contract.as_dict())
+
+            concessionaria = frappe.get_doc('Subsidiary', contract.subsidiaria)
+            context.concessionaria_json = frappe.as_json(concessionaria.as_dict())
+
+            contractedCompany = frappe.get_doc('Contracted Company', doc.contratada)
+            context.contracted_company_json = frappe.as_json(contractedCompany.as_dict())
+
         except frappe.DoesNotExistError:
             frappe.throw(_("Contract Measurement not found"), frappe.DoesNotExistError)
     else:
