c6c12f964515b993d9e7c33934fd4c1066fc9ce1
igor-sumeryan <igor@sumeryan.com>
2025-06-25 12:06:48 +0000

    ...

--- arteris_app/api/contractitem.py
+++ arteris_app/api/contractitem.py
@@ -25,7 +25,7 @@ def create_item_main(contract: str):
     contract_item.descricao = f"Contrato {contract_data['contrato']}"
     contract_item.contrato = contract_data["name"]
     result = contract_item.save()
-    
+
     return result
 
 @frappe.whitelist(methods=["POST"])
@@ -40,3 +40,25 @@ def create_item_main_all_contracts():
         create_item_main(contract.name)
     
     return {"message": "Main items created for all contracts"}
+
+    # contracts = frappe.get_all("Contract", fields=["name"])
+    
+    # for contract in contracts:
+
+    #     contract_doc = frappe.db.get_all("Contract", fields=["name","contrato"], filters={"name": contract.name})
+
+    #     # Make sure we have a contract document
+    #     if not contract_doc:
+    #         frappe.throw("Contract not found")
+
+    #     # Access the first item in the list
+    #     contract_data = contract_doc[0]
+
+    #     contract_item = frappe.new_doc("Contract Item")
+    #     contract_item.old_parent = None
+    #     contract_item.parent_contract_item = None        
+    #     contract_item.is_group = 1
+    #     contract_item.codigo = f"Contrato {contract_data['contrato']}"
+    #     contract_item.descricao = f"Contrato {contract_data['contrato']}"
+    #     contract_item.contrato = contract_data["name"]
+    #     result = contract_item.save()
