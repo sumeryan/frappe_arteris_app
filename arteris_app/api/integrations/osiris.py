e83364c868253d45dda2fbb152963ce151db8989
igor-sumeryan <igor@sumeryan.com>
2025-07-10 16:44:33 +0000

    Desabilitar novo modelo de carga temporariamente

--- arteris_app/api/integrations/osiris.py
+++ arteris_app/api/integrations/osiris.py
@@ -1,15 +1,15 @@
-import frappe
-from . import osiris_client
-from datetime import date, datetime
+# import frappe
+# from . import osiris_client
+# from datetime import date, datetime
 
-@frappe.whitelist(methods=["POST"])
-def get_measurement_records(start_date: str, end_date: str):
+# @frappe.whitelist(methods=["POST"])
+# def get_measurement_records(start_date: str, end_date: str):
     
-    split_start_date = start_date.split("-")
-    split_end_date = end_date.split("-")
+#     split_start_date = start_date.split("-")
+#     split_end_date = end_date.split("-")
 
-    date_start_date = datetime.combine(date(int(split_start_date[0]), int(split_start_date[1]), int(split_start_date[2])), datetime.min.time())
-    date_end_date = datetime.combine(date(int(split_end_date[0]), int(split_end_date[1]), int(split_end_date[2])), datetime.min.time())
+#     date_start_date = datetime.combine(date(int(split_start_date[0]), int(split_start_date[1]), int(split_start_date[2])), datetime.min.time())
+#     date_end_date = datetime.combine(date(int(split_end_date[0]), int(split_end_date[1]), int(split_end_date[2])), datetime.min.time())
 
-    o = osiris_client.OsirisClient(start_date=date_start_date, end_date=date_end_date)
-    o.get_measurement_records()
+#     o = osiris_client.OsirisClient(start_date=date_start_date, end_date=date_end_date)
+#     o.get_measurement_records()
