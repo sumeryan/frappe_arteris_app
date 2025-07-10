a1cba148ada50ec8067548ee4fe86cc012a7252e
igor-sumeryan <igor@sumeryan.com>
2025-07-10 03:20:22 +0000

    Criação do primeiro item do contrato automaticamente

--- arteris_app/arteris/doctype/contract/contract.py
+++ arteris_app/arteris/doctype/contract/contract.py
@@ -3,7 +3,13 @@
 
 # import frappe
 from frappe.model.document import Document
+from arteris_app.api.contractitem import create_item_main
 
 
 class Contract(Document):
-	pass
+	def after_insert(self):
+		# This method is called after the document is inserted into the database
+		# You can add any custom logic here that needs to run after the contract is created
+
+		# Create the main item for the contract
+		create_item_main(self.name)
